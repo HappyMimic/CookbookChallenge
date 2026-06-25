31555de5076a42f82cde3c3660b9c0412d71d165
M. Jones <miriam.j.jones.work@gmail.com>
2026-06-25 14:57:09 -0500

    6.25.26 2.56pm Attempt translation practice. One attempt then shuttering.

---
+++ script.js
@@ -0,0 +1,189 @@
+let PenArmy = [];
+
+const ENtranslatorJapanese = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ja",
+});
+PenArmy.push(ENtranslatorJapanese);
+const ENtranslatorSpanish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "es",
+});
+PenArmy.push(ENtranslatorSpanish);
+const ENtranslatorChinese = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "zh",
+});
+PenArmy.push(ENtranslatorChinese);
+const ENtranslatorDanish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "da",
+});
+PenArmy.push(ENtranslatorDanish);
+const ENtranslatorFijian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "fj",
+});
+PenArmy.push(ENtranslatorFijian);
+const ENtranslatorFinish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "fi",
+});
+PenArmy.push(ENtranslatorFinish);
+const ENtranslatorFrench = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "fr",
+});
+PenArmy.push(ENtranslatorFrench);
+const ENtranslatorGaelic = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "gd",
+});
+PenArmy.push(ENtranslatorGaelic);
+const ENtranslatorGerman = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "de",
+});
+PenArmy.push(ENtranslatorGerman);
+const ENtranslatorGreenlandic = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "kl",
+});
+PenArmy.push(ENtranslatorGreenlandic);
+const ENtranslatorHaitian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ht",
+});
+PenArmy.push(ENtranslatorHaitian);
+const ENtranslatorHebrew = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "he",
+});
+PenArmy.push(ENtranslatorHebrew);
+const ENtranslatorHindi = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "hi",
+});
+PenArmy.push(ENtranslatorHindi);
+const ENtranslatorIcelandic = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "is",
+});
+PenArmy.push(ENtranslatorIcelandic);
+const ENtranslatorIrish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ga",
+});
+PenArmy.push(ENtranslatorIrish);
+const ENtranslatorItalian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "it",
+});
+PenArmy.push(ENtranslatorItalian);
+const ENtranslatorKorean = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ko",
+});
+PenArmy.push(ENtranslatorKorean);
+const ENtranslatorLatin = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "la",
+});
+PenArmy.push(ENtranslatorLatin);
+const ENtranslatorMaori = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "mi",
+});
+PenArmy.push(ENtranslatorMaori);
+const ENtranslatorNepali = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ne",
+});
+PenArmy.push(ENtranslatorNepali);
+const ENtranslatorNorwegian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "no",
+});
+PenArmy.push(ENtranslatorNorwegian);
+const ENtranslatorNorwegianB = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "nb",
+});
+PenArmy.push(ENtranslatorNorwegianB);
+const ENtranslatorNorwegianN = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "nn",
+});
+PenArmy.push(ENtranslatorNorwegianN);
+const ENtranslatorPersian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "fa",
+});
+PenArmy.push(ENtranslatorPersian);
+const ENtranslatorPolish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "pl",
+});
+PenArmy.push(ENtranslatorPolish);
+const ENtranslatorPortuguese = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "pt",
+});
+PenArmy.push(ENtranslatorPortuguese);
+const ENtranslatorRomanian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ro",
+});
+PenArmy.push(ENtranslatorRomanian);
+const ENtranslatorRussian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ru",
+});
+PenArmy.push(ENtranslatorRussian);
+const ENtranslatorSerbian = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "sr",
+});
+PenArmy.push(ENtranslatorSerbian);
+const ENtranslatorSwahili = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "sw",
+});
+PenArmy.push(ENtranslatorSwahili);
+const ENtranslatorSwedish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "sv",
+});
+PenArmy.push(ENtranslatorSwedish);
+const ENtranslatorThai = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "th",
+});
+PenArmy.push(ENtranslatorThai);
+const ENtranslatorTurkish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "tr",
+});
+PenArmy.push(ENtranslatorTurkish);
+const ENtranslatorUrdu = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "ur",
+});
+PenArmy.push(ENtranslatorUrdu);
+const ENtranslatorVietnamese = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "vi",
+});
+PenArmy.push(ENtranslatorVietnamese);
+const ENtranslatorWelsh = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "cy",
+});
+PenArmy.push(ENtranslatorWelsh);
+const ENtranslatorYiddish = await Translator.create({
+    sourceLanguage: "en",
+    targetLanguage: "yi",
+});
+PenArmy.push(ENtranslatorYiddish);
+
+console.log(PenArmy);
